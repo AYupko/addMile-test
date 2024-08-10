5e21cd0f9d55d3e3ce7077fa4a020aa9a710eaff
Yupko <yupkoantondm@gmail.com>
2024-08-10 13:03:06 +0300

    add responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 "use client";
+import cn from "classnames";
 import { Option } from "@/components/option";
 import { Preview } from "@/components/preview";
 import { Step } from "@/components/step";
 import { useGameConfig } from "@/hooks/useGameConfig";
+import { useState } from "react";
 
 export default function Home() {
-  const {
-    preview,
-    currentQuestion,
-    allRewards,
-    onStart,
-    handleAnswer,
-  } = useGameConfig();
+  const [menuActive, setMenuActive] = useState(false);
+
+  const { preview, currentQuestion, allRewards, onStart, handleAnswer } =
+    useGameConfig();
 
   const { question, answers, reward: currReward } = currentQuestion;
 
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
 
   return (
     <main>
@@ -23,16 +25,28 @@ export default function Home() {
       ) : (
         <section className="game">
           <div className="game__main">
+            <a
+              href="#"
+              className={cn("icon", {
+                "icon--close": menuActive,
+                "icon--menu": !menuActive,
+              })}
+              onClick={toggleMenu}
+            ></a>
             <p className="game__question">{question}</p>
             <div className="answers-container">
               {answers.map((answer) => (
-                  <Option answer={answer} checkAnswer={handleAnswer} key={answer.id}/>
+                <Option
+                  answer={answer}
+                  checkAnswer={handleAnswer}
+                  key={answer.id}
+                />
               ))}
             </div>
           </div>
           <aside className="game__ladder">
-            {allRewards.map(reward => (
-              <Step step={reward} currentReward={currReward} key={reward}/>
+            {allRewards.map((reward) => (
+              <Step step={reward} currentReward={currReward} key={reward} />
             ))}
           </aside>
         </section>
